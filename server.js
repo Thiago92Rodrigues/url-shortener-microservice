fcaa318fad655ca2a1838cd5f2345ab16304ec19
th92rodr <thiago.clrodrigues@gmail.com>
2024-05-21 15:34:03 -0300

    update url shortener endpoint

--- server.js
+++ server.js
@@ -1,70 +1,59 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var mongo = require('mongodb');
-var mongoose = require('mongoose');
+var express = require("express");
+var bodyparser = require("body-parser");
+var cors = require("cors");
+var dns = require("dns");
 
-var cors = require('cors');
-var dns = require('dns');
-
-var URLmapping = Map();
+var URLmapping = new Map();
 
 var app = express();
 
-// Basic Configuration 
+// Basic Configuration
 var port = process.env.PORT || 3000;
 
-/** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
-
 app.use(cors());
+app.use(bodyparser.json());
 
-/** this project needs to parse POST bodies **/
-// you should mount the body-parser here
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function(req, res){
-  res.sendFile(process.cwd() + '/views/index.html');
-});
-
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // url shortener endpoint
 app.post("/api/shorturl/new", (req, res) => {
   var { url } = req.body;
-  try {
-    // check if the provided url is valid
-    dns.lookup(url, (error) => {
-      if (error) throw error;
+  var originalUrl = url;
 
-      var n = Math.random();
-      URLmapping.set(n, url);
-  
-      res.json({ "original_url" : url, "short_url" : n });
-    });
-  } catch (error) {
-    res.json({ "error" : "invalid URL" });
-  }
+  // remove trailing '/' from the end of the url
+  url = url.replace(/\/$/, "");
+  // remove the protocol from the beginning of the url
+  url = url.replace(/^https:\/\//, "");
+  url = url.replace(/^http:\/\//, "");
+
+  // check if the provided url is valid
+  dns.lookup(url, (error) => {
+    if (error) {
+      return res.json({ error: "invalid URL" });
+    }
+
+    var n = Math.floor(Math.random() * 100);
+    URLmapping.set(n, originalUrl);
+
+    return res.json({ original_url: originalUrl, short_url: n });
+  });
 });
 
 // access shorted url
 app.get("/api/shorturl/:n", (req, res) => {
   var { n } = req.params;
-  var url = URLmapping.get(n);
+  var url = URLmapping.get(parseInt(n));
 
   // Redirect
-  res.writeHead(302, {
-    'Location' : url
-  });
-  res.end();
+  return res.redirect(url);
 });
 
-
 app.listen(port, function () {
-  console.log('Node.js listening ...');
+  console.log("Node.js listening ...");
 });
